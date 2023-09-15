0ad8ac1add52cc55bbd6904fcb95e9a4093d0caf
isabellysantana807 <127540854+isabellysantana807@users.noreply.github.com>
2023-09-15 15:32:40 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+async function pegarImagens(){
+    const endPoint = 'https://dog.ceo/api/breed/bulldog/images'
+    const response = await fetch(endPoint)
+    const imagens = await response.json()
+    return (imagens.message)
+}
+
+function criarTagImg(imagem){
+    const galeria = document.getElementById('galeria')
+    const tagImg = document.createElement('img')
+    tagImg.src = imagem
+    galeria.appendChild(tagImg)
+}
+
+
+async function carregarFotos(){
+    const imagens = await pegarImagens()
+    imagens.forEach(criarTagImg)
+
+
+}
+
+
+carregarFotos()
